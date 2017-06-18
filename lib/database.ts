b653be8b0bd6a727060e0173d8d0dc7986496e7b
Tomasz Gorka <tomasz@gorka.org.pl>
2017-06-18 19:42:29 +0200

    db facade

--- lib/database.ts
+++ lib/database.ts
@@ -12,7 +12,7 @@ export class RestFacade<T extends Document> extends PlumFacade {
     }
 
     protected async _sync(model: T): Promise<void> {
-        if (!model._kiwiId) {
+        if (!model._syncId) {
             let error = await model.validate();
             if (error) {
                 throw new ValidationPlumError(error);
@@ -37,7 +37,7 @@ export class RestFacade<T extends Document> extends PlumFacade {
     }
 
     public async create(input: { [key: string]: any }): Promise<T> {
-        input._kiwiId = (this.args.syncId) ? this.args.syncId : null;
+        input._syncId = (this.args.syncId) ? this.args.syncId : null;
         let model = new this.Schema(input);
         await this._sync(model);
         return model.save();
@@ -46,9 +46,9 @@ export class RestFacade<T extends Document> extends PlumFacade {
     public async update(conditions: { [key: string]: any }, update: { [key: string]: any }): Promise<T[]> {
         update._modified = Date.now();
         if (this.args.syncId) {
-            update._kiwiId = this.args.syncId;
+            update._syncId = this.args.syncId;
         } else if (this._doSync(update)) {
-            update._kiwiId = null;
+            update._syncId = null;
         }
 
         let models: T[] = await this.find(conditions);
@@ -66,9 +66,9 @@ export class RestFacade<T extends Document> extends PlumFacade {
     public async updateOne(conditions: { [key: string]: any }, update: { [key: string]: any }): Promise<T> {
         update._modified = Date.now();
         if (this.args.syncId) {
-            update._kiwiId = this.args.syncId;
+            update._syncId = this.args.syncId;
         } else if (this._doSync(update)) {
-            update._kiwiId = null;
+            update._syncId = null;
         }
 
         let model: T = await this.findOne(conditions);
@@ -88,9 +88,9 @@ export class RestFacade<T extends Document> extends PlumFacade {
     public async updateById(id: string, update: { [key: string]: any }): Promise<T> {
         update._modified = Date.now();
         if (this.args.syncId) {
-            update._kiwiId = this.args.syncId;
+            update._syncId = this.args.syncId;
         } else if (this._doSync(update)) {
-            update._kiwiId = null;
+            update._syncId = null;
         }
 
         let model = await this.findById(id);
@@ -131,7 +131,7 @@ export class RestFacade<T extends Document> extends PlumFacade {
     public async remove(conditions: { [key: string]: any } = {}): Promise<T[]> {
         return this.update(conditions, {
             _deactivated: Date.now(),
-            _kiwiId: (this.args.syncId) ? this.args.syncId : null
+            _syncId: (this.args.syncId) ? this.args.syncId : null
         });
         //return this.Schema
         //    .findByIdAndRemove(id)
@@ -141,7 +141,7 @@ export class RestFacade<T extends Document> extends PlumFacade {
     public async removeOne(conditions: { [key: string]: any } = {}): Promise<T> {
         return this.updateOne(conditions, {
             _deactivated: Date.now(),
-            _kiwiId: (this.args.syncId) ? this.args.syncId : null
+            _syncId: (this.args.syncId) ? this.args.syncId : null
         });
         //return this.Schema
         //    .findByIdAndRemove(id)
@@ -151,7 +151,7 @@ export class RestFacade<T extends Document> extends PlumFacade {
     public async removeById(id: string): Promise<T> {
         return this.updateById(id, {
             _deactivated: Date.now(),
-            _kiwiId: (this.args.syncId) ? this.args.syncId : null
+            _syncId: (this.args.syncId) ? this.args.syncId : null
         });
         //return this.Schema
         //    .findByIdAndRemove(id)
